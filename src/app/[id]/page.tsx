2b20558eafeae2ce48e34be8a457e9f19ec71e18
Your Name <rasool.expert.freelancer@gmail.com>
2025-01-04 00:07:29 +0500

    update

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,51 +1,40 @@
 import React from "react";
-import { client } from "@/sanity/lib/client";
-import Image from "next/image";
-import { urlFor } from "@/sanity/lib/image";
 import CommentsBox from "../components/CommentsBox";
 import Header from "../components/Header";
 
+interface DataType {
+  id: number;
+  title: string;
+  body: string;
+}
 export default async function Pages({
   params,
 }: {
   params: {
-    id: string;
+    id: number;
   };
 }) {
   const { id } = params;
-  const query = `*[_type == "blog"]{
-  image{
-    asset->{
-      url
-    },
-    alt
-  },
-  blogdate,
-  mainHeading,
-  description
-}`;
-  let data = await client.fetch(query);
-  console.log(data);
+  const api = `https://dummyjson.com/posts`;
+  const fetchdata = await fetch(api);
+  const postdata = await fetchdata.json();
+  const data = postdata.posts;
+  console.log("posr data", data);
+
+
 
   return (
     <>
       <Header />
       <div className="p-10 bg-red-950 w-full h-full flex gap-5 flex-col justify-center items-center">
-        <div className="w-[600px] p-5 flex flex-col gap-10 justify-center items-center bg-[#ececec] rounded-lg text-black">
-          <Image
-            src={urlFor(data[id].image).url()}
-            alt={data[id].image.alt}
-            width={600}
-            height={600}
-          />
-          <div className="w-[550px] py-5">
+        <div className="w-[350px] md:w-[600px] p-5 flex flex-col gap-10 justify-center items-center bg-[#ececec] rounded-lg text-black">
+         
+          <div className="md:w-[550px] w-[350px] py-5">
             <h1 className="font-extrabold text-[24px]">
-              {data[id].mainHeading}
+              {data[id].title}
             </h1>
-            <h3 className="text-[15px] pb-5 text-stone-500 font-semibold">
-              {data[id].blogdate}
-            </h3>
-            <p className="text-[17px]">{data[id].description}</p>
+          
+            <p className="text-[17px]">{data[id].body}</p>
           </div>
         </div>
         <CommentsBox params={{ id }} />

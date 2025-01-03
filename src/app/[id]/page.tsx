0263f25bab1a8d98c83602ef9e378510b520f51a
Your Name <rasool.expert.freelancer@gmail.com>
2025-01-04 00:38:52 +0500

    update

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -2,35 +2,30 @@ import React from "react";
 import CommentsBox from "../components/CommentsBox";
 import Header from "../components/Header";
 
-
 export default async function Pages({
   params,
 }: {
   params: {
-    id: number;
+    id: string; // Change 'id' to string as route params are typically strings
   };
 }) {
-  const { id } = params;
+  // Convert id to a number if necessary
+  const id = parseInt(params.id, 10); // Parse string to number safely
   const api = `https://dummyjson.com/posts`;
   const fetchdata = await fetch(api);
   const postdata = await fetchdata.json();
   const data = postdata.posts;
-  console.log("posr data", data);
 
-
+  console.log("post data", data);
 
   return (
     <>
       <Header />
       <div className="p-10 bg-red-950 w-full h-full flex gap-5 flex-col justify-center items-center">
         <div className="w-[350px] md:w-[600px] p-5 flex flex-col gap-10 justify-center items-center bg-[#ececec] rounded-lg text-black">
-         
           <div className="md:w-[550px] w-[350px] py-5">
-            <h1 className="font-extrabold text-[24px]">
-              {data[id].title}
-            </h1>
-          
-            <p className="text-[17px]">{data[id].body}</p>
+            <h1 className="font-extrabold text-[24px]">{data[id]?.title}</h1>
+            <p className="text-[17px]">{data[id]?.body}</p>
           </div>
         </div>
         <CommentsBox params={{ id }} />
